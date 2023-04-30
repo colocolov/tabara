705c260101361ddc067365df10300365cc5d6f41
Stas Colocolov <colocolov.stas@gmail.com>
2023-04-30 18:52:19 +0300

    first screen and slider

---
+++ #src/js/scripts/heroSlider.js
@@ -0,0 +1,46 @@
+// слайдер на главной
+const heroSlider = new Swiper(".hero__slider", {
+  // loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  //скорость переключения слайдов
+  speed: 800,
+
+
+  // буллеты
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+
+  //эффект перехода слайда (только если показ по 1-му слайду)
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  // переключение при клике на слайд
+  slideToClickedSlide: true,
+  // отключение прокрутки при наведении мыши
+  // on: {
+  //   init() {
+  //     this.el.addEventListener("mouseenter", () => {
+  //       this.autoplay.stop();
+  //     });
+
+  //     this.el.addEventListener("mouseleave", () => {
+  //       this.autoplay.start();
+  //     });
+  //   },
+  // },
+  //
+});
+//----- END
+
+// количество слайдов на главной
+let heightScrollDefault = getComputedStyle(document.documentElement).getPropertyValue('--quantityHeroSlider');
+const quantitySliders = document.querySelector('.hero__pagination').children.length;
+//задаем значание
+document.documentElement.style.setProperty('--quantityHeroSlider', quantitySliders );
