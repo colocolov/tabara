7df933000dc94c8a408faa28a8e155b2bce01c5f
colocolov <colocolov.stas@gmail.com>
2023-05-02 16:14:20 +0300

    Make fullscreen slide

--- #src/js/scripts/slideSite.js
+++ #src/js/scripts/slideSite.js
@@ -1,1 +1,53 @@
-const homePage = document.querySelector('.home-page');
+let homeSlider = new Swiper ('.home', {
+
+  // свои классы
+  wrapperClass: 'home__wrapper',
+  slideClass: 'home__screen',
+
+  // вертикальный слайдер
+  direction: 'vertical',
+
+  // количество слайдов для показа
+  slidesPerView: 'auto',
+
+  // включаем параллакс
+  parallax: true,
+
+  // управление клавиатурой
+  keyboard: {
+    enable: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  // управление колесом мыши
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  // откл функционал, если слайдов меньше чем нужно
+  watchOverflow: true,
+
+  speed: 800,
+
+  // обновить swiper при изменении элементов слайдера
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+
+  // буллеты
+  pagination: {
+    el: '.home__pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'home__bullet',
+    bulletActiveClass: 'home__bullet_active',
+  },
+
+  // скролл
+  scrollbar: {
+    el: '.home__scroll',
+    dragClass: 'home__drag-scroll',
+    draggable: true,
+  },
+});
